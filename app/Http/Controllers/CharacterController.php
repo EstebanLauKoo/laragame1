ce4615f16e972e406bb6efdf9769f7b0c76110c4
Esteban <Esteban_lau_koo@hotmail.com>
2017-06-23 19:19:55 -0400

    working on this

---
+++ app/Http/Controllers/CharacterController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Character;
+use App\Enemies;
+use Illuminate\Http\Request;
+
+class CharacterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('character', ['character' => Character::all(),
+                                    'enemies' => Enemies::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view("form");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $new          = new Character();
+        $new->name    = $request->get( 'Name' );
+        $new->attack =  $request->get( 'Attack');
+        $new->health   =$request->get( 'Health');
+        $new->save();
+
+        return redirect( route( 'Character.create'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Character  $character
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Character $character)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Character  $character
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Character $character)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Character  $character
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Character $character)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Character  $character
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Character $character)
+    {
+        //
+    }
+}
---
+++ database/migrations/2017_06_23_004110_create_character_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCharacterTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('characters', function (Blueprint $table) {
+            $table->string('name');
+            $table->integer('attack');
+            $table->integer('health');
+            $table->timestamp('created_at')->nullable();
+            $table->timestamp('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('characters');
+    }
+}
